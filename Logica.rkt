47d462ed5febade6ac92551837acc142d61b2346
Luis Zuniga <luisandreyzuniga@gmail.com>
2021-03-09 22:20:05 -0600

    Adding function to add cards to players

--- Logica.rkt
+++ Logica.rkt
@@ -10,5 +10,34 @@
     (else (BCEj-aux (cdr List) (append newList (list (list (car List) '() )))))
   )
 )
+(define (current-player-name players)
+  (caar players)
+)
+(define (current-player-cards players)
+  (cadar players)
+)
+(define (other-players players)
+  (cdr players)
+)
+(define (repartir players-list cards-num)
+  (append (other-players players-list) (list (list (current-player-name players-list) (addCard (current-player-cards players-list) cards-num ) )))
+)
+(define (addCard currentCards cardsNumber)
+  (append currentCards (getListNRandom cardsNumber 52))
 
-(BCEj '(Brian Brian2 Brian3))
+)
+(define (getListNRandom n max)
+  (cond
+    ((zero? n) '())
+    (else (append (list (random max)) (getListNRandom (- n 1) max)))
+  )
+)
+
+
+
+
+
+
+
+(addCard '(Brian ()) 2)
+;(BCEj '(Brian Brian2 Brian3))
