878dced4b124db2c61e306afccc9c7d2312e3082
Brian <briwag88@hotmail.com>
2021-03-10 00:18:30 -0600

    se consolida la logica del programa
    se lloro bastante :.v

--- Logica.rkt
+++ Logica.rkt
@@ -1,31 +1,51 @@
 #lang racket 
 (require "Black_Jack_GUI.rkt")
+;lista de jugadores((Crupier (cartas)) (jugado1 (cartas)) (jugador2 (cartas)) ...)
 (define (BCEj List)
-  (BCEj-aux List '((Crupier ())))
-  (start-game)
+  (BCEj-aux List '())
 )
+
 (define (BCEj-aux List newList)
   (cond
     ((empty? List) newList)
-    (else (BCEj-aux (cdr List) (append newList (list (list (car List) '() )))))
+    (else
+     (BCEj-aux
+      (cdr List)
+      (append newList [list[list(car List) '() #t]])
+     )
+    )
   )
 )
+
 (define (current-player-name players)
   (caar players)
 )
+
 (define (current-player-cards players)
   (cadar players)
 )
+
 (define (other-players players)
   (cdr players)
 )
+
 (define (repartir players-list cards-num)
-  (append (other-players players-list) (list (list (current-player-name players-list) (addCard (current-player-cards players-list) cards-num ) )))
+  (append
+   (other-players players-list)
+   [list
+    [list
+     (current-player-name players-list)
+     (addCard (current-player-cards players-list) cards-num)
+    ]
+   ]
+  )
 )
+
 (define (addCard currentCards cardsNumber)
   (append currentCards (getListNRandom cardsNumber 52))
 
 )
+
 (define (getListNRandom n max)
   (cond
     ((zero? n) '())
@@ -33,11 +53,32 @@
   )
 )
 
+(define (start-game list-players turno)
+  
+  (cond
+    ((game-over list-players)
+     {
+      1 ;FINALIZAR JUEGO
+     }
+    )
+   (else
+     1; AGREGAR CARTAS list-players
+   )
+  )
+)
 
-
-
-
-
-
-(addCard '(Brian ()) 2)
-;(BCEj '(Brian Brian2 Brian3))
+(define (game-over list-players)
+  (cond
+    ((empty? list-players)
+     #t
+    )
+    ((equal? (caddar list-players) #t)
+     #f
+    )
+    (else
+     (game-over (cdr list-players))
+    )
+  )
+)  
+;(addCard '(Brian ()) 2)
+(BCEj '(Brian Brian2 Brian3))
